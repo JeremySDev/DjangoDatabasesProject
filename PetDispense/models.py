9f540ddcb4f4df5a559f39841eb251e85496bf85
vagrant <vagrant@ubuntuserver.(none)>
2014-03-13 18:03:13 -0400

    models.py now fully reflects our example

--- PetDispense/models.py
+++ PetDispense/models.py
@@ -1,13 +1,32 @@
 from django.db import models
+import datetime
+import dateutil
 
-# Create your models here.
+
+class Species(models.Model):
+    species_id = models.IntegerField(primary_key=True)
+    species_name = models.CharField(max_length=100, blank=False, null=False)
+
+
+class Breeds(models.Model):
+    breed_id = models.IntegerField(primary_key=True)
+    breed_name = models.CharField(max_length=100, blank=False, null=False)
+    species = models.ForeignKey(Species)
+    #characteristics
+
+
 class AnimalInfo(models.Model):
     animal_id = models.IntegerField(primary_key=True)
     animal_name = models.CharField(max_length=100, blank=False, null=False)
-    species_name = models.ForeignKey(Species.species_name)
-    breed_name = models.ForeignKey(Breeds.breed_name)
-    age = models.IntegerField(max_length=3, blank=False, null=False)
-    in_shelter = models.BooleanField(initial=True, blank=False, null=False)
+    species = models.ForeignKey(Species)
+    breed = models.ForeignKey(Breeds)
+    birth_date = models.DateField('birth date')
+    in_shelter = models.BooleanField(default=True, blank=False, null=False)
+
+    @property
+    def age(self):
+        TODAY = datetime.date.today()
+        return u'%s' % dateutil.relativedelta(TODAY, self.birth_date).years
 
 class Owners(models.Model):
     owner_id = models.IntegerField(primary_key=True)
@@ -15,21 +34,11 @@ class Owners(models.Model):
     owner_firstname = models.CharField(max_length=100, blank=False, null=False)
     owner_phone = models.IntegerField(max_length=10, blank=False, null=False)
     owner_address = models.CharField(max_length=100, blank=False, null=False)
-    animal_id = models.ForeignKey(AnimalInfo.animal_id)
+    animal = models.ForeignKey(AnimalInfo)
+
 
 class MedicalInfo(models.Model):
-     animal_id = models.ForeignKey(AnimalInfo.animal_id)
-     animal_name = models.ForeignKey(AnimalInfo.animal_name)
-     vaccinations = models.CharField(max_length=100, blank=False, null=False)
-     has_illness = models.BooleanField(initial=False, blank=False, null=False)
-     illness = models.CharField(max_length=100, blank=False, null=False)
-
-class Breeds(models.Model):
-    breed_id = models.IntegerField(primary_key=True)
-    breed_name = models.CharField(max_length=100, blank=False, null=False)
-    species_name = models.ForeignKey(Species.species_name)
-    #characteristics
-
-class Species(models.Model):
-    species_id = models.IntegerField(primary_key=True)
-    species_name = models.CharField(max_length=100, blank=False, null=False)
+    animal = models.ForeignKey(AnimalInfo)
+    vaccinations = models.CharField(max_length=100, blank=False, null=False)
+    has_illness = models.BooleanField(default=False, blank=False, null=False)
+    illness = models.CharField(max_length=100, blank=False, null=False)
