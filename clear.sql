64a2d61f1dd6db2aa02e0b8758bd2683463741d4
Jeremy Stilwell <jerstil93@gmail.com>
2014-03-20 11:55:51 -0400

    Delete clear.sql

--- clear.sql
+++
@@ -1,8 +0,0 @@
-BEGIN;
-DROP TABLE "PetDispense_medicalinfo";
-DROP TABLE "PetDispense_owners";
-DROP TABLE "PetDispense_animalinfo";
-DROP TABLE "PetDispense_breeds";
-DROP TABLE "PetDispense_species";
-
-COMMIT;
