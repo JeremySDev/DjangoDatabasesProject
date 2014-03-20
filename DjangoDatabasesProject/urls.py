1cf9593575c751d3d1a9397099884239650864ab
Jeremy Stilwell <jer.dev.stil@gmail.com>
2014-03-20 18:44:18 -0400

    Update webpages

--- DjangoDatabasesProject/urls.py
+++ DjangoDatabasesProject/urls.py
@@ -6,10 +6,11 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-                       url(r'^$', include('PetDispense.urls')),
-                       #url(r'^$', 'PetDispense.urls'),
-                       #url(r'^$', include('PetDispense.urls')),
-                       #url(r'^PetDispense/', include('PetDispense.urls')),
+                       url(r'^PetDispense/', include('PetDispense.urls')),
+
+                       # Uncomment the admin/doc line below to enable admin documentation:
                        url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+                       # Uncomment the next line to enable the admin:
                        url(r'^admin/', include(admin.site.urls)),
 )
