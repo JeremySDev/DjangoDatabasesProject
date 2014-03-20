27212bbd2978e69e489cea9f0e529e6c8af71e9c
Jeremy Stilwell <jer.dev.stil@gmail.com>
2014-03-20 18:20:05 -0400

    updated webpages

--- DjangoDatabasesProject/urls.py
+++ DjangoDatabasesProject/urls.py
@@ -9,7 +9,7 @@ urlpatterns = patterns('',
                        url(r'^$', 'PetDispense.urls'),
                        url(r'^', include('PetDispense.urls')),
                        #url(r'^$', include('PetDispense.urls')),
-                       url(r'^PetDispense/', include('PetDispense.urls')),
+                       #url(r'^PetDispense/', include('PetDispense.urls')),
                        # Uncomment the admin/doc line below to enable admin documentation:
                        url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
