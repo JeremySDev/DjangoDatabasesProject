c36c30b2eb79a428710195cbe787354cc1896378
Jeremy Stilwell <jer.dev.stil@gmail.com>
2014-03-20 19:17:50 -0400

    updated webpages

--- PetDispense/urls.py
+++ PetDispense/urls.py
@@ -5,21 +5,14 @@ from django.conf.urls.static import static
 
 
 urlpatterns = patterns('',
-                       url(r'^index/$', 'PetDispense.views.index', name=u"home"),
                        url(r'^$', 'PetDispense.views.index', name=u"home"),
+                       url(r'^index/$', 'PetDispense.views.index', name=u"home"),
                        url(r'^selection/$', 'PetDispense.views.selection', name=u"selection"),
-                       url(r'^$', 'PetDispense.views.selection', name=u"selection"),
                        url(r'^about/index.html$', 'PetDispense.views.index', name=u"about"),
                        url(r'^about/$', 'PetDispense.views.about', name=u"about"),
-                       url(r'^$', 'PetDispense.views.about', name=u"about"),
                        url(r'^agreement/$', 'PetDispense.views.agreement', name=u"agreement"),
-                       url(r'^$', 'PetDispense.views.agreement', name=u"agreement"),
                        url(r'^confirm/$', 'PetDispense.views.confirm', name=u"confirm"),
-                       url(r'^$', 'PetDispense.views.confirm', name=u"confirm"),
                        url(r'^pin/$', 'PetDispense.views.pin', name=u"pin"),
-                       url(r'^$', 'PetDispense.views.pin', name=u"pin"),
                        url(r'^returns$', 'PetDispense.views.returns', name=u"returns"),
-                       url(r'^$', 'PetDispense.views.returns', name=u"returns"),
                        url(r'^animal$', 'PetDispense.views.animal', name=u"animal"),
-                       url(r'^$', 'PetDispense.views.animal', name=u"animal"),
 ) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
