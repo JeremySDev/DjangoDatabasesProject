380b160d7f760222b4bc15d52f2e49dba82e0bdf
Jeremy Stilwell <jer.dev.stil@gmail.com>
2014-03-20 22:21:31 -0400

    Updated models and tables to create the proper tables in the html

--- PetDispense/tables.py
+++ PetDispense/tables.py
@@ -19,7 +19,7 @@ class BreedsTable(tables.Table):
     class Meta:
         model = Breeds
         sequence = ("breed_id", "breed_name", "species_name")
-        exclude = ("species_id")
+        exclude = ("species_id", "species")
 
 
 class AnimalInfoTable(tables.Table):
