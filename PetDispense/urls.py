8f1ece38c171401287f509c3b58a833de0427ce7
Jeremy Stilwell <jer.dev.stil@gmail.com>
2014-03-21 12:11:21 -0400

    queries

--- PetDispense/urls.py
+++ PetDispense/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import patterns, include, url
 from django.conf import settings
 from django.conf.urls.static import static
 #from django.views.generic import DetailView, ListView
-from PetDispense.views import AnimalList
+from PetDispense.views import AnimalList, AgeList
 
 urlpatterns = patterns('',
                        url(r'^$', 'PetDispense.views.index', name=u"home"),
