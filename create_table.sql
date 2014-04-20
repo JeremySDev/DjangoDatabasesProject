6c9382c198167d1a24264b7b4277b1b27b094daf
Jeremy Stilwell <jerstil93@gmail.com>
2014-04-20 19:04:18 -0400

    polishing html

--- create_table.sql
+++ create_table.sql
@@ -1,40 +1,35 @@
 BEGIN;
 CREATE TABLE "PetDispense_species" (
-    "species_id" integer NOT NULL PRIMARY KEY,
-    "species_name" varchar(100) NOT NULL
-)
-;
+  "species_id"   INTEGER      NOT NULL PRIMARY KEY,
+  "species_name" VARCHAR(100) NOT NULL
+);
 CREATE TABLE "PetDispense_breeds" (
-    "breed_id" integer NOT NULL PRIMARY KEY,
-    "breed_name" varchar(100) NOT NULL,
-    "species_id" integer NOT NULL REFERENCES "PetDispense_species" ("species_id") DEFERRABLE INITIALLY DEFERRED
-)
-;
+  "breed_id"   INTEGER      NOT NULL PRIMARY KEY,
+  "breed_name" VARCHAR(100) NOT NULL,
+  "species_id" INTEGER      NOT NULL REFERENCES "PetDispense_species" ("species_id") DEFERRABLE INITIALLY DEFERRED
+);
 CREATE TABLE "PetDispense_animalinfo" (
-    "animal_id" integer NOT NULL PRIMARY KEY,
-    "animal_name" varchar(100) NOT NULL,
-    "species_id" integer NOT NULL REFERENCES "PetDispense_species" ("species_id") DEFERRABLE INITIALLY DEFERRED,
-    "breed_id" integer NOT NULL REFERENCES "PetDispense_breeds" ("breed_id") DEFERRABLE INITIALLY DEFERRED,
-    "birth_date" date NOT NULL,
-    "in_shelter" boolean NOT NULL
-)
-;
+  "animal_id"   INTEGER      NOT NULL PRIMARY KEY,
+  "animal_name" VARCHAR(100) NOT NULL,
+  "species_id"  INTEGER      NOT NULL REFERENCES "PetDispense_species" ("species_id") DEFERRABLE INITIALLY DEFERRED,
+  "breed_id"    INTEGER      NOT NULL REFERENCES "PetDispense_breeds" ("breed_id") DEFERRABLE INITIALLY DEFERRED,
+  "birth_date"  DATE         NOT NULL,
+  "in_shelter"  BOOLEAN      NOT NULL
+);
 CREATE TABLE "PetDispense_owners" (
-    "owner_id" integer NOT NULL PRIMARY KEY,
-    "owner_lastname" varchar(100) NOT NULL,
-    "owner_firstname" varchar(100) NOT NULL,
-    "owner_phone" integer NOT NULL,
-    "owner_address" varchar(100) NOT NULL,
-    "animal_id" integer NOT NULL REFERENCES "PetDispense_animalinfo" ("animal_id") DEFERRABLE INITIALLY DEFERRED
-)
-;
+  "owner_id"        INTEGER      NOT NULL PRIMARY KEY,
+  "owner_lastname"  VARCHAR(100) NOT NULL,
+  "owner_firstname" VARCHAR(100) NOT NULL,
+  "owner_phone"     INTEGER      NOT NULL,
+  "owner_address"   VARCHAR(100) NOT NULL,
+  "animal_id"       INTEGER      NOT NULL REFERENCES "PetDispense_animalinfo" ("animal_id") DEFERRABLE INITIALLY DEFERRED
+);
 CREATE TABLE "PetDispense_medicalinfo" (
-    "id" serial NOT NULL PRIMARY KEY,
-    "animal_id" integer NOT NULL REFERENCES "PetDispense_animalinfo" ("animal_id") DEFERRABLE INITIALLY DEFERRED,
-    "vaccinations" varchar(100) NOT NULL,
-    "has_illness" boolean NOT NULL,
-    "illness" varchar(100) NOT NULL
-)
-;
+  "id"           SERIAL       NOT NULL PRIMARY KEY,
+  "animal_id"    INTEGER      NOT NULL REFERENCES "PetDispense_animalinfo" ("animal_id") DEFERRABLE INITIALLY DEFERRED,
+  "vaccinations" VARCHAR(100) NOT NULL,
+  "has_illness"  BOOLEAN      NOT NULL,
+  "illness"      VARCHAR(100) NOT NULL
+);
 
 COMMIT;
--- queries.sql
+++ queries.sql
@@ -1,31 +1,95 @@
 --get all the owners firstname and their animals name
-SELECT "PetDispense_animalinfo".animal_id, animal_name, owner_firstname FROM "PetDispense_animalinfo" NATURAL JOIN "PetDispense_owners";
+SELECT
+  "PetDispense_animalinfo".animal_id,
+  animal_name,
+  owner_firstname
+FROM "PetDispense_animalinfo"
+  NATURAL JOIN "PetDispense_owners";
 
 --gets all the pets that have medicine info with there names as well
-SELECT "PetDispense_animalinfo".animal_id, animal_name, vaccinations, has_illness, illness FROM "PetDispense_animalinfo" NATURAL JOIN "PetDispense_medicalinfo";
+SELECT
+  "PetDispense_animalinfo".animal_id,
+  animal_name,
+  vaccinations,
+  has_illness,
+  illness
+FROM "PetDispense_animalinfo"
+  NATURAL JOIN "PetDispense_medicalinfo";
 
 --gets the names of the animal, its species, and its breed
-SELECT "PetDispense_animalinfo".animal_id, animal_name, species_name, breed_name FROM "PetDispense_animalinfo" NATURAL JOIN ("PetDispense_breeds" NATURAL JOIN "PetDispense_species");
+SELECT
+  "PetDispense_animalinfo".animal_id,
+  animal_name,
+  species_name,
+  breed_name
+FROM "PetDispense_animalinfo"
+  NATURAL JOIN ("PetDispense_breeds"
+    NATURAL JOIN "PetDispense_species");
 
 --shows the animals name, its breed, and whether or not its in the shelter
-SELECT "PetDispense_animalinfo".animal_id, in_shelter FROM "PetDispense_animalinfo" NATURAL JOIN "PetDispense_breeds";
+SELECT
+  "PetDispense_animalinfo".animal_id,
+  in_shelter
+FROM "PetDispense_animalinfo"
+  NATURAL JOIN "PetDispense_breeds";
 
 --shows the animals name, whether or not its in the shelter, and if it is ill
-SELECT "PetDispense_animalinfo".animal_id, animal_name, in_shelter, has_illness FROM "PetDispense_animalinfo" NATURAL JOIN "PetDispense_medicalinfo";
+SELECT
+  "PetDispense_animalinfo".animal_id,
+  animal_name,
+  in_shelter,
+  has_illness
+FROM "PetDispense_animalinfo"
+  NATURAL JOIN "PetDispense_medicalinfo";
 
 --finds all animal born before 1995
-SELECT animal_name, breed_name, species_name, birth_date FROM "PetDispense_animalinfo" NATURAL JOIN ("PetDispense_breeds" NATURAL JOIN "PetDispense_species") GROUP BY animal_name, breed_name, species_name, birth_date HAVING birth_date < '1995-01-01';
+SELECT
+  animal_name,
+  breed_name,
+  species_name,
+  birth_date
+FROM "PetDispense_animalinfo"
+  NATURAL JOIN ("PetDispense_breeds"
+    NATURAL JOIN "PetDispense_species")
+GROUP BY animal_name, breed_name, species_name, birth_date
+HAVING birth_date < '1995-01-01';
 
 --find the youngest animal in the shelter
-SELECT animal_name, birth_date, in_shelter FROM "PetDispense_animalinfo" JOIN (SELECT max(birth_date) AS max_age FROM "PetDispense_animalinfo" WHERE in_shelter = 'true') AS "t1" ON t1.max_age = "PetDispense_animalinfo".birth_date;
+SELECT
+  animal_name,
+  birth_date,
+  in_shelter
+FROM "PetDispense_animalinfo"
+  JOIN (SELECT
+          max(birth_date) AS max_age
+        FROM "PetDispense_animalinfo"
+        WHERE in_shelter = 'true') AS "t1" ON t1.max_age = "PetDispense_animalinfo".birth_date;
 
 --sorts by youngest
-SELECT * FROM "PetDispense_animalinfo" ORDER BY birth_date ASC;
+SELECT
+  *
+FROM "PetDispense_animalinfo"
+ORDER BY birth_date ASC;
 
 --finds all cats and dogs
-(SELECT animal_id, animal_name, species_name FROM "PetDispense_animalinfo" NATURAL JOIN "PetDispense_species" WHERE species_name = 'Cat')
+(SELECT
+   animal_id,
+   animal_name,
+   species_name
+ FROM "PetDispense_animalinfo"
+   NATURAL JOIN "PetDispense_species"
+ WHERE species_name = 'Cat')
 UNION
-(SELECT animal_id, animal_name, species_name FROM "PetDispense_animalinfo" NATURAL JOIN "PetDispense_species" WHERE species_name = 'Dog');
+(SELECT
+   animal_id,
+   animal_name,
+   species_name
+ FROM "PetDispense_animalinfo"
+   NATURAL JOIN "PetDispense_species"
+ WHERE species_name = 'Dog');
 
 --how many animal are in the shelter
-SELECT count(*) FROM "PetDispense_animalinfo" WHERE in_shelter = 'true';
+SELECT
+  count(*)
+FROM "PetDispense_animalinfo"
+WHERE in_shelter = 'true';
